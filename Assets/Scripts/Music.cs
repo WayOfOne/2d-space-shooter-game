9c63412bf73521ab9ada0df1c8c4d7b56c26e84f
Donglin Lao <don.lao.1990@gmail.com>
2018-11-26 10:27:46 -0800

    singleton pattern on music

---
+++ Assets/Scripts/Music.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Music : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        SetUpSingleton();
+	}
+
+    private void SetUpSingleton()
+    {
+        // looks for Music type
+        if(FindObjectsOfType(GetType()).Length > 1)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
